4c1154edbb21d4f7f6e53453eba572ef8e19385b
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-07-24 16:30:14 -0600

    ingreso

--- Actividad 06.py
+++ Actividad 06.py
@@ -1,12 +1,30 @@
-def Ingrese_numeros():
-    numeros = input("Que cantidad de numeros desea agregar5?:" )
-    numeropositivo = 0
-    numeronegativo = 0
-    for  i in  range(numeros):
-        sum = input(f"Ingrese el numero{i+1}")
-        sum += numeros
-        if sum > 0:
-            numeropositivo +=1
-        if sum < 0:
-             numeronegativo +=1}
+def ingresar_contar():
+    n = int(input("¿Cuántos números vas a ingresar?: "))
+    suma = 0
+    positivos = 0
+    negativos = 0
 
+    for i in range(n):
+        num = float(input(f"Ingrese el número {i + 1}: "))
+        suma += num
+        if num > 0:
+            positivos += 1
+        elif num < 0:
+            negativos += 1
+    promedio = suma / n if n != 0 else 0
+    return suma, promedio, positivos, negativos
+
+def triangular(base,altura):
+    return base + altura / 2
+
+def numerParoInpar(numeropar):
+    return "par" if numeropar % 2 == 0 else "impar"
+
+def promedioDecalificaciones(n):
+    n = int(input("Cuantas calificaciones se ingresaran?: "))
+    suma = 0
+    for i in range(n):
+     calificacion = float(input(f"ingrese la calificacion{i+1}: "))
+     suma += calificacion
+     promedio = suma / n if n !=0 else 0
+    return promedio
