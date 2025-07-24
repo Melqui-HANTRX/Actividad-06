1e25f856b364591ddc221b463983e4547d4a79f1
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-07-24 09:52:15 -0600

    ingreso

--- Actividad 06.py
+++ Actividad 06.py
@@ -1,3 +1,9 @@
 def ingresar_numeros(n):
+    n = int(input("Cuantos numeros desea agregar?: "))
     numeros = []
-    for i in range(numeros):
+    for i in range(n):
+        suma = float(input(f"Ingrese el numero: {i+1}"))
+        numeros
+
+
+
